bba7d0f4dd4575493ecd449526a1fe83a5dbc453
Dominik Vagala <d.vagala@gmail.com>
2023-02-25 11:47:26 +0100

    update

---
+++ google-colab/rtmdet.py
@@ -0,0 +1,43 @@
+# The new config inherits a base config to highlight the necessary modification
+_base_ = './rtmdet/rtmdet-ins_m_8xb32-300e_coco.py'
+
+# We also need to change the num_classes in head to match the dataset's annotation
+model = dict(
+    bbox_head=dict(num_classes=1)
+    )
+
+# Modify dataset related settings
+data_root = '/content/mmdetection/surfaces/'
+metainfo = {
+    'classes': ('surface', ),
+    'palette': [
+        (220, 20, 60),
+    ]
+}
+train_dataloader = dict(
+    batch_size=1,
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='train/_annotations.coco.json',
+        data_prefix=dict(img='train/')))
+val_dataloader = dict(
+    dataset=dict(
+        data_root=data_root,
+        metainfo=metainfo,
+        ann_file='valid/_annotations.coco.json',
+        data_prefix=dict(img='valid/')))
+test_dataloader = val_dataloader
+
+# Modify metric related settings
+val_evaluator = dict(ann_file=data_root + 'valid/_annotations.coco.json')
+test_evaluator = val_evaluator
+
+# We can use the pre-trained Mask RCNN model to obtain higher performance
+load_from = 'https://download.openmmlab.com/mmdetection/v3.0/rtmdet/rtmdet-ins_m_8xb32-300e_coco/rtmdet-ins_m_8xb32-300e_coco_20221123_001039-6eba602e.pth'
+
+train_cfg = dict(
+    type='EpochBasedTrainLoop',
+    max_epochs=36,
+    val_interval=36,
+    )
